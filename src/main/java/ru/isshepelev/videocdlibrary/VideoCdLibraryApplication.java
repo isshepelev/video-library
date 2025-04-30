9d65ebd8aaea96ee2d392a82a3da4702ba9b1a71
Ilya Shepelev <isshepelev2004@gmail.com>
2025-04-30 18:01:18 +0300

    first commit

---
+++ src/main/java/ru/isshepelev/videocdlibrary/VideoCdLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.videocdlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCdLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCdLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/isshepelev/videocdlibrary/VideoCdLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.videocdlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCdLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
